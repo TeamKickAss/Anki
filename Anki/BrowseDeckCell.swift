c75c0cc8f048ef7c052e3bc3ba293b8c77909472
Isaias Suarez <ijsuarez@wustl.edu>
2016-03-22 21:16:06 -0500

    finished storyboard draft and added viewcontroller files and tablecells

---
+++ Anki/BrowseDeckCell.swift
@@ -0,0 +1,24 @@
+//
+//  BrowseDeckCell.swift
+//  Anki
+//
+//  Created by Labuser on 3/22/16.
+//  Copyright © 2016 TeamKickAss. All rights reserved.
+//
+
+import UIKit
+
+class BrowseDeckCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Anki/ProfileDeckCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileDeckCell.swift
+//  Anki
+//
+//  Created by Labuser on 3/22/16.
+//  Copyright © 2016 TeamKickAss. All rights reserved.
+//
+
+import UIKit
+
+class ProfileDeckCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
